cada55f52b0d3c750fbe6f988bd69e575a0bf137
AdisAsz <106247192+AdisAsz@users.noreply.github.com>
2022-12-14 15:04:32 +0100

    Add files via upload

---
+++ Bot.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main() {
+    
+    time_t current_time = time(nullptr);
+    
+
+   
+    cout << ctime(&current_time);
+
+    return 0;
+}
